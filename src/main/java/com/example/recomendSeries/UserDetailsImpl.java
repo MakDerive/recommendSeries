357092488bc6a449b3075403f4e19927cd672307
MakDerive <kirillandmax21@gmail.com>
2025-04-15 23:42:44 +0300

    Start

---
+++ src/main/java/com/example/recomendSeries/UserDetailsImpl.java
@@ -0,0 +1,7 @@
+package com.example.recomendSeries;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class UserDetailsImpl implements UserDetails{
+	
+}
---
+++ src/test/java/com/example/recomendSeries/RecomendSeriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recomendSeries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecomendSeriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
